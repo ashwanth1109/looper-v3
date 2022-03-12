46282a7d30dec8282d3d57c5f9ddbee783c0cbe1
Ashwanth A R <ashwanth1109@gmail.com>
2022-03-12 12:06:29 +0530

    add menu + avatar to header

--- components/looper/base-ui/header/header.tsx
+++ components/looper/base-ui/header/header.tsx
@@ -1,16 +1,36 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
+import styled from 'styled-components';
+import { Menu } from '@mui/icons-material';
+import { Avatar } from '@mui/material';
 
 export type HeaderProps = {
   /**
    * a node to be rendered in the special component.
    */
-  children?: ReactNode;
+  children?: React.ReactNode;
 };
+
+const Container = styled.div`
+  width: 100%;
+  height: 56px;
+  padding: 0 16px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #202020;
+  box-sizing: border-box;
+
+  svg.MuiSvgIcon-root {
+    color: white;
+  }
+`;
 
 export function Header({ children }: HeaderProps) {
   return (
-    <div>
-      {children}
-    </div>
+    <Container>
+      <Menu />
+      <Avatar>AR</Avatar>
+    </Container>
   );
 }
